a5aae7ff9395f15b2a7559b9451103c109b474ac
Harish Sethi <sethi.harish@gmail.com>
2020-07-19 22:46:39 +1000

    Add migration InitialCreate to create Banner, Category and Pie database tables

---
+++ OnlineShop.Data.Sql.Migrations/Scripts/Patch/20200719113208_InitialCreate.sql
@@ -0,0 +1,56 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Banner] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [Description] nvarchar(max) NULL,
+    [ImageUrl] nvarchar(max) NULL,
+    CONSTRAINT [PK_Banner] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Category] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [Description] nvarchar(max) NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Pie] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [Price] decimal(18,2) NOT NULL,
+    [ShortDescription] nvarchar(max) NULL,
+    [LongDescription] nvarchar(max) NULL,
+    [InStock] bit NOT NULL,
+    [IsPieOfTheWeek] bit NOT NULL,
+    [ImageUrl] nvarchar(max) NULL,
+    [ThumbnailImageUrl] nvarchar(max) NULL,
+    [CategoryId] int NOT NULL,
+    CONSTRAINT [PK_Pie] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Pie_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_Pie_CategoryId] ON [Pie] ([CategoryId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200719113208_InitialCreate', N'3.1.6');
+
+GO
+
---
+++ OnlineShop.Data.Sql.Migrations/Scripts/Rollback/20200719113208_InitialCreate.sql
@@ -0,0 +1,17 @@
+﻿DROP TABLE [Banner];
+
+GO
+
+DROP TABLE [Pie];
+
+GO
+
+DROP TABLE [Category];
+
+GO
+
+DELETE FROM [__EFMigrationsHistory]
+WHERE [MigrationId] = N'20200719113208_InitialCreate';
+
+GO
+
