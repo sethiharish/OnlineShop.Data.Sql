89f8a960c62c9b4ab03f41ba123037ca0b4b1ede
Harish Sethi <sethi.harish@gmail.com>
2020-07-19 22:44:56 +1000

    Set up EF migrations

---
+++ OnlineShop.Data.Sql/OnlineShopContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace OnlineShop.Data.Sql
+{
+    class OnlineShopContextFactory : IDesignTimeDbContextFactory<OnlineShopContext>
+    {
+        public OnlineShopContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<OnlineShopContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                options => options.MigrationsAssembly("OnlineShop.Data.Sql.Migrations"));
+
+            return new OnlineShopContext(optionsBuilder.Options);
+        }
+    }
+}
