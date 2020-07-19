f8ee0da543f375ca371a6f3db028463952a1d7a9
Harish Sethi <sethi.harish@gmail.com>
2020-07-19 20:53:07 +1000

    Add Banner, Category and Pie services

---
+++ OnlineShop.Data.Sql/OnlineShopContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Entities;
+
+namespace OnlineShop.Data.Sql
+{
+    public class OnlineShopContext : DbContext
+    {
+        public OnlineShopContext(DbContextOptions<OnlineShopContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Banner> Banner { get; set; }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Pie> Pie { get; set; }
+    }
+}
