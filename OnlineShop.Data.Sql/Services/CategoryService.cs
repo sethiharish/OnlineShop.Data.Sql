504667b6d8dc20a2ecb7a47e68b6ce8879c7f169
Harish Sethi <sethi.harish@gmail.com>
2020-07-20 14:42:46 +1000

    Add Get Category By Name method

--- OnlineShop.Data.Sql/Services/CategoryService.cs
+++ OnlineShop.Data.Sql/Services/CategoryService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data.Services;
 using OnlineShop.Entities;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -23,6 +24,12 @@ namespace OnlineShop.Data.Sql.Services
         public async Task<Category> GetCategoryAsync(int categoryId)
         {
             return await context.Category.FindAsync(categoryId);
+        }
+
+        public async Task<Category> GetCategoryByNameAsync(string category)
+        {
+            return await context.Category.FirstOrDefaultAsync(c => 
+                                            c.Name.Equals(category, StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
