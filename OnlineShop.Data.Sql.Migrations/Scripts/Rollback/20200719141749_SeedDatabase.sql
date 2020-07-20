b4ddb71efe2fe7032cf765ba2258abe6ca33ade1
Harish Sethi <sethi.harish@gmail.com>
2020-07-20 13:15:04 +1000

    Add seed database migration

---
+++ OnlineShop.Data.Sql.Migrations/Scripts/Rollback/20200719141749_SeedDatabase.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM Pie;
+
+GO
+
+DELETE FROM Category;
+
+GO
+
+DELETE FROM Banner;
+
+GO
+
+DELETE FROM [__EFMigrationsHistory]
+WHERE [MigrationId] = N'20200719141749_SeedDatabase';
+
+GO
+
